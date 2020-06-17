08ce072f0159d300313e4150ac4426174d567632
Mr fu <61105005+Mrrfu@users.noreply.github.com>
2020-06-17 10:51:42 +0800

    添加了一些注释

--- cpp_test2/zuheclear.cpp
+++ cpp_test2/zuheclear.cpp
@@ -16,12 +16,12 @@ zuheclear::~zuheclear()
     delete ui;
 }
 
-void zuheclear::on_pushButton_2_clicked()
+void zuheclear::on_pushButton_2_clicked()//点击后取消清除
 {
     close();
 }
 
-void zuheclear::on_pushButton_clicked()
+void zuheclear::on_pushButton_clicked()//点击后进行清理
 {
     m1_parent->clear3();//调用清理函数
    close();
